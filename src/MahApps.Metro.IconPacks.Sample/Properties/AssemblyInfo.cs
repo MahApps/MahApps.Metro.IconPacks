5885b144a4bc4c09522be412768ae392fa3e8d8e
Yoker.Wu <Yoker.Wu@gmail.com>
2016-08-05 09:55:51 +0800

    add Sample project for IconPacks

---
+++ src/MahApps.Metro.IconPacks.Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyTitle("MahApps.Metro.IconPacks.Sample")]
+[assembly: AssemblyDescription("Seach & Preview Sample for IconPacks")]
+[assembly: AssemblyCompany("MahApps")]
+[assembly: AssemblyProduct("MahApps.Metro.IconPacks.Sample")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+
