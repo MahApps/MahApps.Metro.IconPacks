dc67ee59f3b0f645cb64608603dc699cc0acd0e9
Jan Karger <jan.karger@inform-software.com>
2016-09-20 10:11:06 +0200

    Add raw base frame of MahApps.Metro.IconPacks.Browser project

---
+++ src/MahApps.Metro.IconPacks.Browser/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace MahApps.Metro.IconPacks.Browser
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : MetroWindow
+    {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
