af9675a3631cb05a5639aea6233c5eb0836ab2c3
Jan Karger <jan.karger@inform-software.com>
2016-09-21 14:42:54 +0200

    Filtering stuff

--- src/MahApps.Metro.IconPacks.Browser/ViewModels/IconPackViewModel.cs
+++ src/MahApps.Metro.IconPacks.Browser/ViewModels/IconPackViewModel.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Windows.Data;
 
 namespace MahApps.Metro.IconPacks.Browser.ViewModels
 {
     public class IconPackViewModel : ViewModelBase
     {
         private IEnumerable<IIconViewModel> _icons;
+        private ICollectionView _iconsCollectionView;
+        private string _filterText;
+        private IIconViewModel _selectedIcon;
 
         public IconPackViewModel(string caption, Type iconPackType)
         {
             this.Caption = caption;
             this.Icons = GetIcons(iconPackType);
+            this.PrepareFiltering();
+            this.SelectedIcon = this.Icons.First();
+        }
+
+        private void PrepareFiltering()
+        {
+            this._iconsCollectionView = CollectionViewSource.GetDefaultView(this.Icons);
+            this._iconsCollectionView.Filter = o => this.FilterIconsPredicate(this.FilterText, (IIconViewModel) o);
+        }
+
+        private bool FilterIconsPredicate(string filterText, IIconViewModel iconViewModel)
+        {
+            return string.IsNullOrWhiteSpace(filterText)
+                   || iconViewModel.Name.IndexOf(filterText, StringComparison.CurrentCultureIgnoreCase) >= 0;
         }
 
         private static string GetDescription(Enum value)
@@ -46,6 +64,30 @@ namespace MahApps.Metro.IconPacks.Browser.ViewModels
             {
                 if (Equals(value, _icons)) return;
                 _icons = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FilterText
+
+        {
+            get { return _filterText; }
+            set
+            {
+                if (value == _filterText) return;
+                _filterText = value;
+                OnPropertyChanged();
+                this._iconsCollectionView.Refresh();
+            }
+        }
+
+        public IIconViewModel SelectedIcon
+        {
+            get { return _selectedIcon; }
+            set
+            {
+                if (Equals(value, _selectedIcon)) return;
+                _selectedIcon = value;
                 OnPropertyChanged();
             }
         }
