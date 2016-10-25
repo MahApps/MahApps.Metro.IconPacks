20a574c9dc0c3c4d9bf0254e212431aa9594ee6c
Jan Karger <punkerat76@gmail.com>
2016-10-25 16:44:32 +0200

    Some Browser changes   - search/filter over all icons and include the description   - use background for icons on top and don't use UseLayoutRounding   - set version to 1.2.0

--- src/MahApps.Metro.IconPacks.Browser/ViewModels/MainViewModel.cs
+++ src/MahApps.Metro.IconPacks.Browser/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Reflection;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace MahApps.Metro.IconPacks.Browser.ViewModels
 {
@@ -9,11 +11,23 @@ namespace MahApps.Metro.IconPacks.Browser.ViewModels
         private string _appVersion;
         private string _iconPacksVersion;
         private ICommand _goToGitHubCommand;
+        private Dispatcher _dispatcher;
+        private string _filterText;
 
-        public MainViewModel()
+        public MainViewModel(Dispatcher dispatcher)
         {
-            AppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            IconPacksVersion = Assembly.GetAssembly(typeof(PackIconMaterial)).GetName().Version.ToString();
+            this._dispatcher = dispatcher;
+            this.AppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            this.IconPacks = new ObservableCollection<IconPackViewModel>(
+                new[]
+                {
+                    new IconPackViewModel(this, "Material", typeof(PackIconMaterialKind), typeof(PackIconMaterial)),
+                    new IconPackViewModel(this, "FontAwesome", typeof(PackIconFontAwesomeKind), typeof(PackIconFontAwesome)),
+                    new IconPackViewModel(this, "Octicons", typeof(PackIconOcticonsKind), typeof(PackIconOcticons)),
+                    new IconPackViewModel(this, "Modern", typeof(PackIconModernKind), typeof(PackIconModern)),
+                    new IconPackViewModel(this, "Entypo+", typeof(PackIconEntypoKind), typeof(PackIconEntypo))
+                });
+            this.IconPacksVersion = Assembly.GetAssembly(typeof(PackIconMaterial)).GetName().Version.ToString();
             this.GoToGitHubCommand =
                 new SimpleCommand
                 {
@@ -22,46 +36,53 @@ namespace MahApps.Metro.IconPacks.Browser.ViewModels
                 };
         }
 
-        public ObservableCollection<IconPackViewModel> IconPacks { get; set; } = new ObservableCollection<IconPackViewModel>(
-            new[]
-            {
-                new IconPackViewModel("Material", typeof(PackIconMaterialKind), typeof(PackIconMaterial)),
-                new IconPackViewModel("FontAwesome", typeof(PackIconFontAwesomeKind), typeof(PackIconFontAwesome)),
-                new IconPackViewModel("Octicons", typeof(PackIconOcticonsKind), typeof(PackIconOcticons)),
-                new IconPackViewModel("Modern", typeof(PackIconModernKind), typeof(PackIconModern)),
-                new IconPackViewModel("Entypo+", typeof(PackIconEntypoKind), typeof(PackIconEntypo))
-            });
+        public ObservableCollection<IconPackViewModel> IconPacks { get; set; }
 
         public string AppVersion
         {
-            get { return _appVersion; }
+            get { return this._appVersion; }
             set
             {
-                if (value == _appVersion) return;
-                _appVersion = value;
-                OnPropertyChanged();
+                if (value == this._appVersion) return;
+                this._appVersion = value;
+                this.OnPropertyChanged();
             }
         }
 
         public string IconPacksVersion
         {
-            get { return _iconPacksVersion; }
+            get { return this._iconPacksVersion; }
             set
             {
-                if (value == _iconPacksVersion) return;
-                _iconPacksVersion = value;
-                OnPropertyChanged();
+                if (value == this._iconPacksVersion) return;
+                this._iconPacksVersion = value;
+                this.OnPropertyChanged();
             }
         }
 
         public ICommand GoToGitHubCommand
         {
-            get { return _goToGitHubCommand; }
+            get { return this._goToGitHubCommand; }
             set
             {
-                if (Equals(value, _goToGitHubCommand)) return;
-                _goToGitHubCommand = value;
+                if (Equals(value, this._goToGitHubCommand)) return;
+                this._goToGitHubCommand = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public string FilterText
+        {
+            get { return _filterText; }
+            set
+            {
+                if (value == _filterText) return;
+                _filterText = value;
                 OnPropertyChanged();
+                foreach (var iconPack in this.IconPacks)
+                {
+                    this._dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => iconPack.FilterText = value));
+                }
             }
         }
     }
