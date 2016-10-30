1f1185abd113d0f9c35eae6476370d026e7c5527
Jan Karger <punkerat76@gmail.com>
2016-10-30 21:21:50 +0100

    Add the other separate packs (but it doesn't works yet, need a core dll)

---
+++ src/MahApps.Metro.IconPacks.UWP.Octicons/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyTitle("MahApps.Metro.IconPacks.Octicons")]
+[assembly: AssemblyDescription("IconPacks for stylish awesome UI WPF apps.")]
+[assembly: AssemblyCompany("MahApps")]
