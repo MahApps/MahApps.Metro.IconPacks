333c66e71ab973e7c4e8a426001249a6c5940e60
Jan Karger <punkerat76@gmail.com>
2016-10-30 17:22:10 +0100

    First working full package UWP

--- src/MahApps.Metro.IconPacks/PackIconMaterial.cs
+++ src/MahApps.Metro.IconPacks/PackIconMaterial.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿
+#if !NETFX_CORE
+using System.Windows;
+#endif
 
 namespace MahApps.Metro.IconPacks
 {
@@ -6,13 +9,19 @@ namespace MahApps.Metro.IconPacks
     /// Icons from the Material Design Icons project, <see><cref>https://materialdesignicons.com/</cref></see>.
     /// </summary>
     public class PackIconMaterial : PackIcon<PackIconMaterialKind>
-    {        
+    {
+#if !NETFX_CORE
         static PackIconMaterial()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(PackIconMaterial), new FrameworkPropertyMetadata(typeof(PackIconMaterial)));
-        }     
+        }
+#endif
 
         public PackIconMaterial() : base(PackIconMaterialDataFactory.Create)
-        { }    
+        {
+#if NETFX_CORE
+            this.DefaultStyleKey = typeof(PackIconMaterial);
+#endif
+        }
     }
 }
