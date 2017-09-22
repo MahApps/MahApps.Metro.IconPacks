2e3a0d4358e947b15619423217cade128bdbe568
Jan Karger <punkerat76@gmail.com>
2017-09-22 17:03:31 +0200

    rename extensions (remove PackIcon prefix)

--- src/MahApps.Metro.IconPacks/PackIconSimpleIconsExtension.cs
+++ src/MahApps.Metro.IconPacks/PackIconSimpleIconsExtension.cs
@@ -3,13 +3,13 @@
 namespace MahApps.Metro.IconPacks
 {
     [MarkupExtensionReturnType(typeof(PackIconSimpleIcons))]
-    public class PackIconSimpleIconsExtension : PackIconExtension<PackIconSimpleIcons, PackIconSimpleIconsKind>
+    public class SimpleIconsExtension : PackIconExtension<PackIconSimpleIcons, PackIconSimpleIconsKind>
     {
-        public PackIconSimpleIconsExtension()
+        public SimpleIconsExtension()
         {
         }
 
-        public PackIconSimpleIconsExtension(PackIconSimpleIconsKind kind) : base(kind)
+        public SimpleIconsExtension(PackIconSimpleIconsKind kind) : base(kind)
         {
         }
     }
