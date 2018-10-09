1c9cb73931f667bf8408c4db3ea586f0ae7c061c
Jan Karger <punkerat76@gmail.com>
2018-10-10 00:21:20 +0200

    add Typicons v2.0.9

---
+++ src/MahApps.Metro.IconPacks/PackIconTypicons.cs
@@ -0,0 +1,28 @@
+﻿
+#if !NETFX_CORE
+using System.Windows;
+#endif
+
+namespace MahApps.Metro.IconPacks
+{
+    /// <summary>
+    /// Typicons Icons/Artwork distributed under [CC BY-SA](<see><cref>https://creativecommons.org/licenses/by-sa/3.0/</cref></see>) licence.
+    /// Typicons Font distributed under 'SIL Open Font License' licence.
+    /// </summary>
+    public class PackIconTypicons : PackIconControl<PackIconTypiconsKind>
+    {
+#if !NETFX_CORE
+        static PackIconTypicons()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PackIconTypicons), new FrameworkPropertyMetadata(typeof(PackIconTypicons)));
+        }
+#endif
+
+        public PackIconTypicons() : base(PackIconTypiconsDataFactory.Create)
+        {
+#if NETFX_CORE
+            this.DefaultStyleKey = typeof(PackIconTypicons);
+#endif
+        }
+    }
+}
---
+++ src/MahApps.Metro.IconPacks/PackIconTypiconsExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Markup;
+
+namespace MahApps.Metro.IconPacks
+{
+    [MarkupExtensionReturnType(typeof(PackIconTypicons))]
+    public class TypiconsExtension : PackIconExtension<PackIconTypicons, PackIconTypiconsKind>
+    {
+        public TypiconsExtension()
+        {
+        }
+
+        public TypiconsExtension(PackIconTypiconsKind kind) : base(kind)
+        {
+        }
+    }
+}
