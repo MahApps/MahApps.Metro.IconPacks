c2a99bf19f5b2d3222951d8ef9297fccb97d23cf
punker76 <punkerat76@gmail.com>
2019-02-19 00:21:18 +0100

    some fixes for output

--- src/MahApps.Metro.IconPacks/PathIconFeatherIcons.cs
+++ src/MahApps.Metro.IconPacks/PathIconFeatherIcons.cs
@@ -2,11 +2,13 @@
 
 namespace MahApps.Metro.IconPacks
 {
+    /*
     /// <summary>
+    /// FeatherIcons not working for now together with PathIcon, because PathIcon doesn't allow to set Stroke related properties.
+    /// 
     /// Feather is licensed under the MIT License <see><cref>https://github.com/feathericons/feather/blob/master/LICENSE</cref></see>
     /// Contributions, corrections and requests can be made on GitHub <see><cref>https://github.com/feathericons/feather</cref></see>.
     /// </summary>
-    /* FeatherIcons not working for now together with PathIcon, because PathIcon doesn't allow to set Stroke related properties.
     public class PathIconFeatherIcons : PathIconControl<PackIconFeatherIconsKind>
     {
         public PathIconFeatherIcons() : base(PackIconFeatherIconsDataFactory.Create)
