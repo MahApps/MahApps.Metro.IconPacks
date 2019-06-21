a2e2fb12281a6ce8125ef779a9b527e3787c2f71
punker76 <punkerat76@gmail.com>
2019-06-21 23:57:35 +0200

    (GH-132) MarkupExtension for UWP 16299

--- src/MahApps.Metro.IconPacks/PackIconJamIconsExtension.cs
+++ src/MahApps.Metro.IconPacks/PackIconJamIconsExtension.cs
@@ -1,8 +1,20 @@
-﻿using System.Windows.Markup;
+﻿#if (NETFX_CORE || WINDOWS_UWP)
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+#else
+using System.Windows.Markup;
+using System.Windows.Media.Animation;
+#endif
 
 namespace MahApps.Metro.IconPacks
 {
+#if (NETFX_CORE || WINDOWS_UWP)
+    [MarkupExtensionReturnType(ReturnType = typeof(PackIconJamIcons))]
+#else
     [MarkupExtensionReturnType(typeof(PackIconJamIcons))]
+#endif
     public class JamIconsExtension : PackIconExtension<PackIconJamIcons, PackIconJamIconsKind>
     {
         public JamIconsExtension()
--- src/MahApps.Metro.IconPacks/PackIconZondiconsExtension.cs
+++ src/MahApps.Metro.IconPacks/PackIconZondiconsExtension.cs
@@ -1,8 +1,20 @@
-﻿using System.Windows.Markup;
+﻿#if (NETFX_CORE || WINDOWS_UWP)
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+#else
+using System.Windows.Markup;
+using System.Windows.Media.Animation;
+#endif
 
 namespace MahApps.Metro.IconPacks
 {
+#if (NETFX_CORE || WINDOWS_UWP)
+    [MarkupExtensionReturnType(ReturnType = typeof(PackIconZondicons))]
+#else
     [MarkupExtensionReturnType(typeof(PackIconZondicons))]
+#endif
     public class ZondiconsExtension : PackIconExtension<PackIconZondicons, PackIconZondiconsKind>
     {
         public ZondiconsExtension()
