e92d69b5602d55b056df436fa46992ad0bf54231
punker76 <punkerat76@gmail.com>
2019-09-27 22:34:01 +0200

    Add missing BoxIconsExtension

---
+++ src/MahApps.Metro.IconPacks/PackIconBoxIconsExtension.cs
@@ -0,0 +1,28 @@
+﻿#if (NETFX_CORE || WINDOWS_UWP)
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+#else
+using System.Windows.Markup;
+using System.Windows.Media.Animation;
+#endif
+
+namespace MahApps.Metro.IconPacks
+{
+#if (NETFX_CORE || WINDOWS_UWP)
+    [MarkupExtensionReturnType(ReturnType = typeof(PackIconBoxIcons))]
+#else
+    [MarkupExtensionReturnType(typeof(PackIconBoxIcons))]
+#endif
+    public class BoxIconsExtension : PackIconExtension<PackIconBoxIcons, PackIconBoxIconsKind>
+    {
+        public BoxIconsExtension()
+        {
+        }
+
+        public BoxIconsExtension(PackIconBoxIconsKind kind) : base(kind)
+        {
+        }
+    }
+}
