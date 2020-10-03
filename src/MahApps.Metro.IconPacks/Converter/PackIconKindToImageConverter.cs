87ad3f3e6de76139811ee818fbd4978d36c812f3
punker76 <punkerat76@gmail.com>
2020-10-03 17:05:00 +0200

    Add RemixIcon v2.5.0

--- src/MahApps.Metro.IconPacks/Converter/PackIconKindToImageConverter.cs
+++ src/MahApps.Metro.IconPacks/Converter/PackIconKindToImageConverter.cs
@@ -55,6 +55,9 @@ namespace MahApps.Metro.IconPacks.Converter
                 case PackIconPixelartIconsKind pixelartIconsKind:
                     PackIconPixelartIconsDataFactory.DataIndex.Value?.TryGetValue(pixelartIconsKind, out data);
                     return data;
+                case PackIconRemixIconKind remixIconKind:
+                    PackIconRemixIconDataFactory.DataIndex.Value?.TryGetValue(remixIconKind, out data);
+                    return data;
                 case PackIconRPGAwesomeKind rpgAwesomeKind:
                     PackIconRPGAwesomeDataFactory.DataIndex.Value?.TryGetValue(rpgAwesomeKind, out data);
                     return data;
---
+++ src/MahApps.Metro.IconPacks/PackIconRemixIconExtension.cs
@@ -0,0 +1,40 @@
+﻿#if (NETFX_CORE || WINDOWS_UWP)
+using Windows.UI.Xaml.Markup;
+#else
+using System;
+using System.Windows.Markup;
+#endif
+
+namespace MahApps.Metro.IconPacks
+{
+#if (NETFX_CORE || WINDOWS_UWP)
+    [MarkupExtensionReturnType(ReturnType = typeof(PackIconRemixIcon))]
+#else
+    [MarkupExtensionReturnType(typeof(PackIconRemixIcon))]
+#endif
+    public class RemixIconExtension : BasePackIconExtension
+    {
+        public RemixIconExtension()
+        {
+        }
+
+#if !(NETFX_CORE || WINDOWS_UWP)
+        public RemixIconExtension(PackIconRemixIconKind kind)
+        {
+            this.Kind = kind;
+        }
+
+        [ConstructorArgument("kind")]
+#endif
+        public PackIconRemixIconKind Kind { get; set; }
+
+#if (NETFX_CORE || WINDOWS_UWP)
+        protected override object ProvideValue()
+#else
+        public override object ProvideValue(IServiceProvider serviceProvider)
+#endif
+        {
+            return this.GetPackIcon<PackIconRemixIcon, PackIconRemixIconKind>(this.Kind);
+        }
+    }
+}
