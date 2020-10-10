6777a28070afebd18ebb84a1d7e050d1b573dfb2
punker76 <punkerat76@gmail.com>
2020-10-10 18:10:09 +0200

    Genrated PackIconControlDataFactory

--- src/MahApps.Metro.IconPacks/PackIconControlDataFactory.cs
+++ src/MahApps.Metro.IconPacks/PackIconControlDataFactory.cs
@@ -25,112 +25,90 @@ namespace MahApps.Metro.IconPacks
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconEntypoDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconEvaIconsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconFeatherIconsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconFontAwesomeDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconIoniconsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconJamIconsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
-            foreach (var packIcon in PackIconMaterialDataFactory.Create())
-            {
-                dictionary.Add(packIcon.Key, packIcon.Value);
-            }
-
             foreach (var packIcon in PackIconMaterialDesignDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
+            foreach (var packIcon in PackIconMaterialDataFactory.Create())
+            {
+                dictionary.Add(packIcon.Key, packIcon.Value);
+            }
             foreach (var packIcon in PackIconMaterialLightDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconMicronsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconModernDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconOcticonsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconPicolIconsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconPixelartIconsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconRemixIconDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconRPGAwesomeDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconSimpleIconsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconTypiconsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconUniconsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconWeatherIconsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             foreach (var packIcon in PackIconZondiconsDataFactory.Create())
             {
                 dictionary.Add(packIcon.Key, packIcon.Value);
             }
-
             return dictionary;
         }
     }
