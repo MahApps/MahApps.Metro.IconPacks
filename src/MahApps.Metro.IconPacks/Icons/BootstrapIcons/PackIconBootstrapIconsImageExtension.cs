7db58a876d27ca8d06a7c49ab38f904362bfb0c8
punker76 <punkerat76@gmail.com>
2020-10-12 00:05:56 +0200

    Add Bootstrap Icons v1.0.0

---
+++ src/MahApps.Metro.IconPacks/Icons/BootstrapIcons/PackIconBootstrapIconsImageExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace MahApps.Metro.IconPacks
+{
+    [MarkupExtensionReturnType(typeof(ImageSource))]
+    public class BootstrapIconsImageExtension : BasePackIconImageExtension
+    {
+        public BootstrapIconsImageExtension()
+        {
+        }
+
+        public BootstrapIconsImageExtension(PackIconBootstrapIconsKind kind)
+        {
+            this.Kind = kind;
+        }
+
+        [ConstructorArgument("kind")]
+        public PackIconBootstrapIconsKind Kind { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return CreateImageSource(this.Kind, this.Brush ?? Brushes.Black);
+        }
+
+        /// <inheritdoc />
+        protected override string GetPathData(object iconKind)
+        {
+            string data = null;
+            if (iconKind is PackIconBootstrapIconsKind kind)
+            {
+                PackIconBootstrapIconsDataFactory.DataIndex.Value?.TryGetValue(kind, out data);
+            }
+            return data;
+        }
+
+        /// <inheritdoc />
+        protected override ScaleTransform GetScaleTransform(object iconKind)
+        {
+            return new ScaleTransform(1, -1);
+        }
+    }
+}
