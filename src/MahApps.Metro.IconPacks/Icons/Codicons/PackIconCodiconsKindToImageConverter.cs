7b0bd93c6a0813a49081cb76cd50548f70798860
punker76 <punkerat76@gmail.com>
2020-10-12 12:21:44 +0200

    Add Codicons v0.0.12

---
+++ src/MahApps.Metro.IconPacks/Icons/Codicons/PackIconCodiconsKindToImageConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Media;
+
+namespace MahApps.Metro.IconPacks.Converter
+{
+    public class PackIconCodiconsKindToImageConverter : PackIconKindToImageConverterBase
+    {
+        /// <inheritdoc />
+        protected override string GetPathData(object iconKind)
+        {
+            string data = null;
+            if (iconKind is PackIconCodiconsKind kind)
+            {
+                PackIconCodiconsDataFactory.DataIndex.Value?.TryGetValue(kind, out data);
+            }
+            return data;
+        }
+
+        /// <inheritdoc />
+        protected override ScaleTransform GetScaleTransform(object iconKind)
+        {
+            return new ScaleTransform(1, -1);
+        }
+    }
+}
