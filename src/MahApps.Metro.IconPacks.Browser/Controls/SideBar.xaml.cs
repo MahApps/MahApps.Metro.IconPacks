448c7adc1438f58a23e224989e786aac41293a5d
Tim <tim.uniewski@gmail.com>
2020-11-22 22:26:50 +0100

    Add experimental UWP Export

--- src/MahApps.Metro.IconPacks.Browser/Controls/SideBar.xaml.cs
+++ src/MahApps.Metro.IconPacks.Browser/Controls/SideBar.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.IconPacks.Browser.Model;
 using MahApps.Metro.IconPacks.Browser.Properties;
 using MahApps.Metro.IconPacks.Browser.ViewModels;
 using Microsoft.Win32;
@@ -22,8 +23,9 @@ namespace MahApps.Metro.IconPacks.Browser.Controls
 
         public SideBar()
         {
-            SaveAsSvg_Command = new SimpleCommand((_) => SaveAsSvg_Execute(),(_) => DataContext is IIconViewModel);
-            SaveAsWpf_Command = new SimpleCommand((_) => SaveAsWpf_Execute(),(_) => DataContext is IIconViewModel);
+            SaveAsSvg_Command = new SimpleCommand((_) => SaveAsSvg_Execute(), (_) => DataContext is IIconViewModel);
+            SaveAsWpf_Command = new SimpleCommand((_) => SaveAsWpf_Execute(), (_) => DataContext is IIconViewModel);
+            SaveAsUwp_Command = new SimpleCommand((_) => SaveAsUwp_Execute(), (_) => DataContext is IIconViewModel);
             InitializeComponent();
         }
 
@@ -79,18 +81,22 @@ namespace MahApps.Metro.IconPacks.Browser.Controls
                     (T.M22*(T.OffsetY - (bBox.Top - T.M22*(svgSize - bBox.Height)/2)) + (bBox.Height - T.M22 * bBox.Height) / 2).ToString(CultureInfo.InvariantCulture)
                 });
 
+                var parameters = new ExportParameters()
+                {
+                    FillColor = iconPath.Fill is Brush ? "black" : "none",
+                    PageHeight = svgSize.ToString(CultureInfo.InvariantCulture),
+                    PageWidth = svgSize.ToString(CultureInfo.InvariantCulture),
+                    PathData = iconContol.Data,
+                    StrokeColor = iconPath.Stroke is Brush ? "black" : "none",
+                    StrokeWidth = iconPath.Stroke is null ? "0" : iconPath.StrokeThickness.ToString(CultureInfo.InvariantCulture),
+                    StrokeLineCap = iconPath.StrokeEndLineCap.ToString().ToLower(),
+                    StrokeLineJoin = iconPath.StrokeLineJoin.ToString().ToLower(),
+                    TranformMatrix = transform
+                };
+
                 var svgFileTemplate = io.File.ReadAllText(io.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ExportTemplates", "SVG.xml"));
 
-                svgFileContent = svgFileTemplate
-                    .Replace("{{PageWidth}}", svgSize.ToString(CultureInfo.InvariantCulture))
-                    .Replace("{{PageHeight}}", svgSize.ToString(CultureInfo.InvariantCulture))
-                    .Replace("{{PathData}}", iconContol.Data)
-                    .Replace("{{FillColor}}", iconPath.Fill is Brush ? "black" : "none")
-                    .Replace("{{StrokeColor}}", iconPath.Stroke is Brush ? "black" : "none")
-                    .Replace("{{StrokeWidth}}", iconPath.Stroke is null ? "0" : iconPath.StrokeThickness.ToString(CultureInfo.InvariantCulture))
-                    .Replace("{{StrokeLineCap}}", iconPath.StrokeEndLineCap.ToString().ToLower())
-                    .Replace("{{StrokeLineJoin}}", iconPath.StrokeLineJoin.ToString().ToLower())
-                    .Replace("{{TranformMatrix}}", transform);
+                svgFileContent = ExportHelper.FillTemplate(svgFileTemplate, parameters);
 
                 using (io.StreamWriter file = new io.StreamWriter(fileSaveDialog.FileName))
                 {
@@ -127,32 +133,81 @@ namespace MahApps.Metro.IconPacks.Browser.Controls
                 var bBox = iconPath.Data.Bounds;
 
                 var xamlSize = Math.Max(bBox.Width, bBox.Height);
-                var T = iconPath.LayoutTransform.Value;
+                var T = iconPath.LayoutTransform.Value; 
 
-                var transform = T.ToString(CultureInfo.InvariantCulture);
-                    
-                //    string.Join(",", new[]
-                //{
-                //    T.M11.ToString(CultureInfo.InvariantCulture),
-                //    T.M21.ToString(CultureInfo.InvariantCulture),
-                //    T.M12.ToString(CultureInfo.InvariantCulture),
-                //    T.M22.ToString(CultureInfo.InvariantCulture),
-                //    (T.M11*(T.OffsetX - bBox.Left + T.M11*(svgSize - bBox.Width)/2) + (bBox.Width - T.M11 * bBox.Width) / 2).ToString(CultureInfo.InvariantCulture),
-                //    (T.M22*(T.OffsetY - (bBox.Top - T.M22*(svgSize - bBox.Height)/2)) + (bBox.Height - T.M22 * bBox.Height) / 2).ToString(CultureInfo.InvariantCulture)
-                //});
 
                 var wpfFileTemplate = io.File.ReadAllText(io.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ExportTemplates", "WPF.xml"));
 
-                wpfFileContent = wpfFileTemplate
-                    .Replace("{{PageWidth}}", xamlSize.ToString(CultureInfo.InvariantCulture))
-                    .Replace("{{PageHeight}}", xamlSize.ToString(CultureInfo.InvariantCulture))
-                    .Replace("{{PathData}}", iconContol.Data)
-                    .Replace("{{FillColor}}", iconPath.Fill is Brush ? "Black" : "{x:Null}")
-                    .Replace("{{StrokeColor}}", iconPath.Stroke is Brush ? "Black" : "{x:Null}")
-                    .Replace("{{StrokeWidth}}", iconPath.Stroke is null ? "0" : iconPath.StrokeThickness.ToString(CultureInfo.InvariantCulture))
-                    .Replace("{{StrokeLineCap}}", iconPath.StrokeEndLineCap.ToString())
-                    .Replace("{{StrokeLineJoin}}", iconPath.StrokeLineJoin.ToString())
-                    .Replace("{{TranformMatrix}}", transform);
+                var parameters = new ExportParameters()
+                {
+                    FillColor = iconPath.Fill is Brush ? "Black" : "{x:Null}",
+                    PageHeight = xamlSize.ToString(CultureInfo.InvariantCulture),
+                    PageWidth = xamlSize.ToString(CultureInfo.InvariantCulture),
+                    PathData = iconContol.Data,
+                    StrokeColor = iconPath.Stroke is Brush ? "Black" : "{x:Null}",
+                    StrokeWidth = iconPath.Stroke is null ? "0" : iconPath.StrokeThickness.ToString(CultureInfo.InvariantCulture),
+                    StrokeLineCap = iconPath.StrokeEndLineCap.ToString().ToLower(),
+                    StrokeLineJoin = iconPath.StrokeLineJoin.ToString().ToLower(),
+                    TranformMatrix = T.ToString(CultureInfo.InvariantCulture)
+                };
+
+                wpfFileContent = ExportHelper.FillTemplate(wpfFileTemplate, parameters);
+
+
+                using (io.StreamWriter file = new io.StreamWriter(fileSaveDialog.FileName))
+                {
+                    file.Write(wpfFileContent);
+                }
+
+            }
+            await progress.CloseAsync();
+        }
+
+        public ICommand SaveAsUwp_Command { get; }
+        private async void SaveAsUwp_Execute()
+        {
+            var progress = await this.TryFindParent<MetroWindow>()?.ShowProgressAsync("Export", "Saving selected icon as WPF-XAML-file");
+            progress.SetIndeterminate();
+
+            var fileSaveDialog = new SaveFileDialog()
+            {
+                AddExtension = true,
+                DefaultExt = "xaml",
+                FileName = $"{SelectedIcon?.IconPackType.Name.Remove(0, 8)}-{SelectedIcon.Name}",
+                Filter = "UWP-XAML (*.xaml)|*.xaml",
+                OverwritePrompt = true
+            };
+
+            if (fileSaveDialog.ShowDialog() == true)
+            {
+                string wpfFileContent;
+
+                var iconContol = PreviewHolder.FindChild<PackIconControlBase>();
+                var iconPath = PreviewHolder.FindChild<Path>();
+
+                var bBox = iconPath.Data.Bounds;
+
+                var xamlSize = Math.Max(bBox.Width, bBox.Height);
+                var T = iconPath.LayoutTransform.Value;
+
+
+                var wpfFileTemplate = io.File.ReadAllText(io.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ExportTemplates", "UWP.xml"));
+
+                var parameters = new ExportParameters()
+                {
+                    FillColor = iconPath.Fill is Brush ? "Black" : "{x:Null}",
+                    PageHeight = xamlSize.ToString(CultureInfo.InvariantCulture),
+                    PageWidth = xamlSize.ToString(CultureInfo.InvariantCulture),
+                    PathData = iconContol.Data,
+                    StrokeColor = iconPath.Stroke is Brush ? "Black" : "{x:Null}",
+                    StrokeWidth = iconPath.Stroke is null ? "0" : iconPath.StrokeThickness.ToString(CultureInfo.InvariantCulture),
+                    StrokeLineCap = iconPath.StrokeEndLineCap.ToString().ToLower(),
+                    StrokeLineJoin = iconPath.StrokeLineJoin.ToString().ToLower(),
+                    TranformMatrix = T.ToString(CultureInfo.InvariantCulture)
+                };
+
+                wpfFileContent = ExportHelper.FillTemplate(wpfFileTemplate, parameters);
+
 
                 using (io.StreamWriter file = new io.StreamWriter(fileSaveDialog.FileName))
                 {
---
+++ src/MahApps.Metro.IconPacks.Browser/Model/ExportHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahApps.Metro.IconPacks.Browser.Model
+{
+    internal class ExportHelper
+    {
+        // SVG-File
+        private static string _SvgFileTemplate;
+        internal static string SvgFileTemplate => _SvgFileTemplate ??= File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ExportTemplates", "SVG.xml"));
+
+
+        // XAML-File (WPF)
+        private static string _WpfFileTemplate;
+        internal static string WpfFileTemplate => _WpfFileTemplate ??= File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ExportTemplates", "WPF.xml"));
+
+        // XAML-File (WPF)
+        private static string _UwpFileTemplate;
+        internal static string UwpFileTemplate => _UwpFileTemplate ??= File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ExportTemplates", "WPF.xml"));
+
+
+        internal static string FillTemplate(string template, ExportParameters parameters) 
+        {
+            return template.Replace("{{IconKind}}",parameters.IconKind)
+                           .Replace("{{IconPackName}}",parameters.IconPackName)
+                           .Replace("{{IconPackHomepage}}",parameters.IconPackHomepage)
+                           .Replace("{{IconPackLicense}}",parameters.IconPackLicense)
+                           .Replace("{{PageWidth}}", parameters.PageWidth)
+                           .Replace("{{PageHeight}}", parameters.PageHeight)
+                           .Replace("{{PathData}}", parameters.PathData)
+                           .Replace("{{FillColor}}", parameters.FillColor)
+                           .Replace("{{StrokeColor}}", parameters.StrokeColor)
+                           .Replace("{{StrokeWidth}}", parameters.StrokeWidth)
+                           .Replace("{{StrokeLineCap}}", parameters.StrokeLineCap)
+                           .Replace("{{StrokeLineJoin}}", parameters.StrokeLineJoin)
+                           .Replace("{{TranformMatrix}}", parameters.TranformMatrix);
+        }
+    }
+
+
+    internal struct ExportParameters
+    {
+        internal string IconKind { get; set; }
+        internal string IconPackName { get; set; }
+        internal string IconPackHomepage { get; set; }
+        internal string IconPackLicense { get; set; }
+        internal string PageWidth { get; set; }
+        internal string PageHeight { get; set; }
+        internal string PathData { get; set; }
+        internal string FillColor { get; set; }
+        internal string StrokeColor { get; set; }
+        internal string StrokeWidth { get; set; }
+        internal string StrokeLineCap { get; set; }
+        internal string StrokeLineJoin { get; set; }
+        internal string TranformMatrix { get; set; }
+    }            
+}
