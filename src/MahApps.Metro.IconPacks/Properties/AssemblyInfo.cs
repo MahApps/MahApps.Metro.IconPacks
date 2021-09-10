edfe6ea6a4ccaaf3f4cba1b7f88f28705318ddbc
punker76 <punkerat76@gmail.com>
2021-09-10 11:22:09 +0200

    Add XmlnsPrefix

--- src/MahApps.Metro.IconPacks/Properties/AssemblyInfo.cs
+++ src/MahApps.Metro.IconPacks/Properties/AssemblyInfo.cs
@@ -4,8 +4,9 @@ using System.Windows;
 using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: XmlnsDefinition("http://metro.mahapps.com/winfx/xaml/iconpacks", "MahApps.Metro.IconPacks")]
-[assembly: XmlnsDefinition("http://metro.mahapps.com/winfx/xaml/iconpacks", "MahApps.Metro.IconPacks.Converter")]
+[assembly: XmlnsPrefix(@"http://metro.mahapps.com/winfx/xaml/iconpacks", "iconpacks")]
+[assembly: XmlnsDefinition(@"http://metro.mahapps.com/winfx/xaml/iconpacks", "MahApps.Metro.IconPacks")]
+[assembly: XmlnsDefinition(@"http://metro.mahapps.com/winfx/xaml/iconpacks", "MahApps.Metro.IconPacks.Converter")]
 #endif
 
 [assembly: ComVisible(false)]
