4103ec51d146f672d02310335a4771b3d55eb44b
punker76 <punkerat76@gmail.com>
2025-10-31 10:17:50 +0100

    feat: update BoxIcons v2 and add BoxIcons v3

---
+++ src/MahApps.Metro.IconPacks.BoxIcons2/PackIconBoxIcons2KindToImageConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Media;
+
+namespace MahApps.Metro.IconPacks.Converter
+{
+    public class PackIconBoxIcons2KindToImageConverter : PackIconKindToImageConverterBase
+    {
+        /// <inheritdoc />
+        protected override string GetPathData(object iconKind)
+        {
+            string data = null;
+            if (iconKind is PackIconBoxIcons2Kind kind)
+            {
+                PackIconDataFactory<PackIconBoxIcons2Kind>.DataIndex.Value?.TryGetValue(kind, out data);
+            }
+            return data;
+        }
+
+        /// <inheritdoc />
+        protected override ScaleTransform GetScaleTransform(object iconKind)
+        {
+            return new ScaleTransform(1, -1);
+        }
+    }
+}
